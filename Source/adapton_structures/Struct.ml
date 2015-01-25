0695020b91e9edea8d96f3aed859d722e8234df2
Kyle Headley <kheadley@umd.edu>
2015-01-25 18:55:32 -0500

    remove old code

--- Source/adapton_structures/Struct.ml
+++ Source/adapton_structures/Struct.ml
@@ -11,7 +11,6 @@ module type StructType = sig
   module ArtLib : ArtLibType
   module Name : NameType
   module Data : DatType
-  module Art : ArtType
 
   (* 1+R:(a)+D:(d,s)+A:(n,a)+B:(n,a,s)+C:(s) *)
   type 'art art_struct = [ (* articulated struct. *)
@@ -51,7 +50,6 @@ module MakeCommonStruct
   module ArtLib = ArtLib
   module Name = Name
   module Data = Data
-  module Art = ArtLib.MakeArt(Name)(Data)
 
   type 'art art_struct = [ (* articulated sequence. *)
   | `Nil
@@ -115,13 +113,8 @@ module MakeCommonStruct
     module Art = ArtLib.MakeArt(Name)(Data)
   end
 
-(* 
-  module AData       = ArtLib.MakeArt(Name)(Data)
-  module ADataOption = ArtLib.MakeArt(Name)(Types.Option(Data))
- *)
-
   (* articulation module for data items *)
-  module DArt = Art
+  module DArt = ArtLib.MakeArt(Name)(Data)
   (* articulation module for whole structure *)
   module SArt = Datastruct.Art
 
@@ -223,187 +216,14 @@ module MakeSequence
   module ArtLib = Common.ArtLib
   module Name = Common.Name
   module Data = Common.Data
-  module Art = Common.Art
+
   module SeqData = Common.Datastruct.Data
 
-  type t = Art.t Common.art_struct
+  type t = Common.Datastruct.Art.t Common.art_struct
+
+  (* articulation module for whole structure *)
+  module SArt = Common.Datastruct.Art
 
   let more_funs = ()
 
 end
-
-(* some old data for reference
-  (* ---------- first attempt at mutable list ----------- *)
-
-  (* creates an articulated list *)
-  let art_list
-    ?g:(granularity=default_granularity)
-    (input_list : St.Data.t list)
-    : St.List.Data.t
-  =
-    let rec loop l =
-      match l with
-      | [] -> `Nil
-      | x::xs ->
-        if ffs (St.Data.hash 0 x) >= granularity then
-          let nm1, nm2 = Name.fork (Name.nondet()) in
-          `Cons(x, `Name(nm1, `Art (LArt.cell nm2 (loop xs))))
-        else
-          `Cons(x, (loop xs))
-    in
-    loop input_list
-
-  (* returns a standard list *)
-  let to_list (list : St.List.Data.t) : St.Data.t list = 
-    let rec loop l =
-      match l with
-      | `Nil -> []
-      | `Art(a) -> loop (LArt.force a)
-      | `Name(_, xs) -> loop xs
-      | `Cons(x, xs) -> x::(loop xs)
-    in
-    loop list
-
-  (* inserts an element at the beginning of the list *)
-  let list_cons
-    ?g:(granularity=default_granularity)
-    (h : St.Data.t)
-    (tl : St.List.Data.t)
-  =
-    if ffs (St.Data.hash 0 h) >= granularity then
-      let nm1, nm2 = Name.fork (Name.nondet()) in
-      `Cons(h, `Name(nm1, `Art (LArt.cell nm2 tl)))
-    else
-      `Cons(h, tl)
-
-  (* returns head and tail of list *)
-  let list_snoc
-    (list : St.List.Data.t)
-    : (St.Data.t * St.List.Data.t) option
-  =
-    let rec loop l = 
-      match l with
-      | `Nil -> None
-      | `Art(a) -> loop (LArt.force a)
-      | `Name(_, xs) -> loop xs
-      | `Cons(x, xs) -> Some(x, xs)
-    in
-    loop list
-
-
-   (* --------------------------- *) 
-
-  let mut_elms_of_list
-    ( name : Name.t )
-    ( list : 'a list )
-    ( data_of : 'a -> St.Data.t )
-    ( name_of : 'a -> Name.t ) 
-    ( gran_level : int )
-    : St.List.Art.t
-  = 
-    let rec loop list =
-      match list with
-      | [] -> `Nil
-      | x :: xs ->
-        if ffs (St.Data.hash 0 (data_of x)) >= gran_level then
-          let nm1, nm2 = Name.fork (name_of x) in
-          `Cons((data_of x), `Name(nm1, `Art (St.List.Art.cell nm2 (loop xs))))
-        else
-          `Cons((data_of x), (loop xs))
-    in St.List.Art.cell name (loop list)
-
-  let rec insert_elm list_art h nm_tl_opt =
-    match nm_tl_opt with
-    | Some (nm, tl_art) ->
-      let list_art_content = St.List.Art.force list_art in
-      St.List.Art.set list_art (`Cons(h, `Name(nm, `Art(tl_art)))) ;
-      St.List.Art.set tl_art list_art_content
-    | None ->
-      let list_art_content = St.List.Art.force list_art in
-      St.List.Art.set list_art (`Cons(h, list_art_content))      
-
-  let rec delete_elm list_art =
-    let (x,x_tl) =
-      let rec loop list = 
-        match list with
-        | `Art art ->
-          let elm, tl = loop (St.List.Art.force art) in
-          elm, (`Art art)
-            
-        | `Name (nm, tl) ->
-          let elm, tl = loop tl in
-          elm, (`Name(nm, tl))
-            
-        | `Cons(x, tl) -> (x,tl)
-        | `Nil -> failwith "delete_elm: Nil: No element to delete"
-      in
-      loop (St.List.Art.force list_art)
-    in
-    St.List.Art.set list_art x_tl ;
-    (x,x_tl)
-
-  let rec next_art x = match x with
-    | `Nil -> None
-    | `Cons(_, tl) -> next_art tl
-    | `Name(_, rest) -> next_art rest
-    | `Art a -> Some a
-
-  let rec next_cons x =
-    match x with
-    | `Nil -> None
-    | `Cons(x,xs) -> Some(x,xs)
-    | `Art(a) -> next_cons(LArt.force a)
-    | `Name(_, xs) -> next_cons xs
-
-  let rec ith_art list count = 
-    ( match count with
-    | x when x <= 0 -> list
-    | _ -> match list with
-      | `Nil -> `Nil
-      | `Cons(x, xs) -> ith_art xs (count-1)
-      | `Name(_, xs) -> ith_art xs count
-      | `Art a -> ith_art (St.List.Art.force a) count
-    )
-
-  let rec take list count =
-    let dec = function
-      | Some count -> Some (count-1)
-      | None -> None
-    in
-    ( match count with
-    | Some count when (count <= 0) -> []
-    | _ ->       
-      match list with
-      | `Nil -> []
-      | `Cons(x, xs) -> x :: (take xs (dec count))
-      | `Name(_, xs) -> take xs count
-      | `Art a -> take (St.List.Art.force a) count
-    )
-
-  let rec list_is_empty ( list : St.List.Data.t) : bool =
-    ( match list with
-    | `Nil -> true
-    | `Cons(_,_) -> false
-    | `Art a -> list_is_empty ( LArt.force a )
-    | `Name (_,x) -> list_is_empty x
-    )
-
-  let list_append = 
-    let mfn = LArt.mk_mfn (St.Name.gensym "list_append")
-      (module Types.Tuple2(St.List.Data)(St.List.Data))
-      (fun r (xs, ys) ->
-        let list_append xs ys = r.LArt.mfn_data (xs,ys) in
-        ( match xs with
-        | `Nil -> ys
-        | `Cons(x,tl) -> `Cons(x, list_append tl ys)
-        | `Art a -> list_append (LArt.force a) ys
-        | `Name(nm,xs) -> 
-          let nm1, nm2 = Name.fork nm in
-          `Name(nm1, `Art (r.LArt.mfn_nart nm2 (xs, ys)))
-        ))
-    in
-    fun xs ys -> mfn.LArt.mfn_data (xs, ys)
-
-end
-
-*)
