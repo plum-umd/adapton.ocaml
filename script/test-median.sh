b606b64da8654e485b998fbe8dc2b81e2e42dc17
Kyle Headley <kheadley@umd.edu>
2015-02-24 21:23:10 -0500

    few more gran

--- script/test-median.sh
+++ script/test-median.sh
@@ -17,7 +17,7 @@ for len in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000; do
     #ver - adapton version that is being tested, as well as the test set.
     for ver in name arggen lazyrecalceagernoninc ; do 
       #gran - 2^n average distance between articulation points
-      for gran in 0 1 2 3 4 5 6 7 8 9; do
+      for gran in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
         #samp - sample number, is the random seed that is used to create the initial data to be modified
         for samp in 1 2 3 4 5 6 7; do
           fullver=Rope_median_$ver
