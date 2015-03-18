bc56d8fb32359a0e6536542ddef8daf5ddc5b327
Kyle Headley <kheadley@umd.edu>
2015-03-18 00:35:12 -0400

    work on quickhull, needs Rope_filter

--- Source/test/quickhull.ml
+++ Source/test/quickhull.ml
@@ -58,22 +58,25 @@ let line_point_distance : line -> point -> float =
       let cross = cross_product diff1 diff2 in
       if cross <= 0.0 then false else true
 
-module StMake (St : SpreadTree.SpreadTreeType with type Data.t = Types.Int) 
+module StMake (IntsSt : SpreadTree.SpreadTreeType 
+  (* with type Data.t = Types.Int) *)
+)
 = struct
 
-  module Name = St.Name
+  module Name = IntsSt.Name
+  module ArtLib = IntsSt.ArtLib
 
   (* type points = point list *)
-  module pointsSt = SpreadTree.MakeSpreadTree
-    (St.ArtLib)(St.Name)(Types.Tuple2(Types.Float,Types.Float))
-  module PList = pointsSt.Rope.Art
-  module AccumList = pointsSt.List.Art
-  module Seq = SpreadTree.MakeSeq(pointsSt)
-  module Point = Seq.Adata
+  module PointsSt = SpreadTree.MakeSpreadTree
+    (ArtLib)(Name)(Types.Tuple2(Types.Float)(Types.Float))
+  module PList = PointsSt.Rope.Art
+  module AccumList = PointsSt.List.Art
+  module Seq = SpreadTree.MakeSeq(PointsSt)
+  module Point = PointsSt.Data
+  (* to provide a point with an associated name *)
+  module PointName = ArtLib.MakeArt(Name)(Types.Tuple2(Point)(Name))
 
-  type aline = Point.t * Point.t
-                     
-  let furthest_point_from_line : aline -> PList.t -> Point.t =
+  let furthest_point_from_line : line -> PList.t -> PointName.t =
     (* Used in the "pivot step".  the furthest point defines the two
        lines that we use for the "filter step".
        Note: To make this into an efficient IC algorithm, need to use a
@@ -81,33 +84,31 @@ module StMake (St : SpreadTree.SpreadTreeType with type Data.t = Types.Int)
        iterative list reduction.  *)
     fun (l1,l2) points ->
       let max_point pt1 pt2 = 
-        let d1 = line_point_distance (Point.force l1, Point.force l2) pt1 in
-        let d2 = line_point_distance (Point.force l1, Point.force l2) pt2 in
-        if d1 > d2 then d1 else d2
+        let d1 = line_point_distance (l1, l2) pt1 in
+        let d2 = line_point_distance (l1, l2) pt2 in
+        if d1 > d2 then pt1 else pt2
       in
-      let nm1, nm2 = Name.fork (Name.gensym "fpfl")
-      let reduce = Seq.rope_reduce nm1 max_point in
-      Point.thunk(nm2)(fun () -> 
-        match reduce (PArt.force points) with
-        | None -> failwith "no points"
-        | Some(x) -> x
+      let nm1, nm2 = Name.fork (Name.gensym "fpfl") in
+      let reduce = Seq.rope_reduce_name nm1 max_point in
+      PointName.thunk(nm2)(fun () -> 
+        match reduce (PList.force points) with
+        | None, _ -> failwith "no points"
+        | _, None -> failwith "no name"
+        | Some(x), Some(nm) -> x, nm
+      )
 
 
-  (*
-    There's a number of problems with this as is:
-      Where do names come from? (extracted with pivot_point from points?)
-      Should the output be a lazy sequence, with arts that track its construction,
-      or should this just be a memoized fun, with the output needing to be reforced?
-      (it would compose better as a lazy sequence)
-      How does an accumulator work incrementally? (it doesn't have internal recursive calls!)
-  *)
-  let quickhull_rec : aline -> PList.t -> AccumList.t -> AccumList.t =
+  let quickhull_rec : line -> PList.t -> AccumList.t -> AccumList.t =
     (* Adapton: Use a memo table here.  Our accumulator, hull_accum, is
        a nominal list.  We need to use names because otherwise, the
        accumulator will be unlikely to match after a small change. *)
-    let module QH = St.ArtLib.MakeArt(Name)(AccumList.t) in
-    let mfn = QH.mk_mfn (St.Name.gensym "quick_hull")
-    (module Types.Tuple3(Types.Tuple2(St.Data,St.data))(pointsSt.Rope.Data)(pointsSt.List))
+    let module QH = ArtLib.MakeArt(Name)(AccumList) in
+    let mfn = QH.mk_mfn (Name.gensym "quick_hull")
+    (module Types.Tuple3
+      (Types.Tuple2(PointsSt.Data)(PointsSt.Data))
+      (PointsSt.Rope.Data)
+      (PointsSt.List.Data)
+    )
     (fun r ((p1,p2) as line, points, hull_accum) ->
       let quickhull l p h = r.QH.mfn_data (l,p,h) in
       let filter = Seq.rope_filter in
@@ -115,18 +116,20 @@ module StMake (St : SpreadTree.SpreadTreeType with type Data.t = Types.Int)
       | `Zero -> hull_accum
       | _ ->
         let points_above_line = filter 
-          (line_side_test (Point.force p1, Point.force p2))
+          (line_side_test (p1, p2))
           (PList.force points)
         in
-        let pivot_point = furthest_point_from_line line points in
+        let pivot_point, p_nm = PointName.force (furthest_point_from_line line points) in
+        let nm1, nm2 = Name.fork p_nm in
         let l_line = (p1, pivot_point) in
         let r_line = (pivot_point, p2) in
-        let hull_accum = `Cons(Point.force pivot_point,
+        let hull_accum = `Cons(pivot_point,
           `Name(nm1, `Art(QH.mfn_nart nm2 (
             quickhull_rec r_line points_above_line hull_accum)))
         ) in
         quickhull_rec l_line points_above_line hull_accum
-
+    ) in
+    fun l p h -> mfn.QH.mfn_data (l,p,h)
 
   (* 
     for this fun, we need to:
@@ -136,7 +139,7 @@ module StMake (St : SpreadTree.SpreadTreeType with type Data.t = Types.Int)
     This should probably be done in two steps, one interfacing
     with experiments.ml and the other as the main routine
   *)
-  let quickhull : points -> points =
+(*   let quickhull : points -> points =
     (* A convex hull consists of an upper and lower hull, each computed
        recursively using quickhull_rec.  We distinguish these two
        sub-hulls using an initial line that is defined by the points
@@ -147,5 +150,5 @@ module StMake (St : SpreadTree.SpreadTreeType with type Data.t = Types.Int)
     let hull = quickhull_rec (p_min_x, p_max_x) [p_max_x] points in
     let hull = quickhull_rec (p_max_x, p_min_x) (p_min_x :: hull) points in
     hull
-
+ *)
 end
