a72ccc863d2e7a08dde56f64768e73c2375c996c
Kyle Headley <kheadley@umd.edu>
2015-03-25 16:35:53 -0400

    mergesort arggen

--- script/test-oopsla15.sh
+++ script/test-oopsla15.sh
@@ -251,32 +251,33 @@ TESTER=../experiments.native
 #   done
 # done
 
-# FILE=$FILE_PRE/mergesort_linechart.csv
-# #len - length of the list, must be a multiple of --num-changes, which has a default of 10
-# for len in 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000; do
-#   #samp - sample number, is the random seed that is used to create the initial data to be modified
-#   for samp in 1 2 3 4 5 6 7; do
-#     #gran- granularity, average articulation chunk size is 2^gran 
-#     for gran in 0; do
-#       #dem - amount of the list that is demended
-#       for dem in 100.0; do
-#         #ver - adapton version that is being tested, as well as the test set.
-#         for ver in name lazyrecalc; do 
-#           #change - particular change that is done to the list, default is all
-#           for change in "--id"; do
-#             fullver=Rope_mergesort_$ver
-#             echo "Running: $fullver @ $len @ $samp"
-#             args1="--sample-num $samp --n $len --demand $dem"
-#             args2="--experiment $fullver --gran $gran"
-#             args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
-#             args4= #"--test-flags" #check for correctness
-#             $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
-#           done
-#         done
-#       done
-#     done
-#   done
-# done
+FILE=$FILE_PRE/mergesort_linechart.csv
+#len - length of the list, must be a multiple of --num-changes, which has a default of 10
+for len in 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000; do
+  #samp - sample number, is the random seed that is used to create the initial data to be modified
+  for samp in 1 2 3 4 5 6 7; do
+    #gran- granularity, average articulation chunk size is 2^gran 
+    for gran in 0; do
+      #dem - amount of the list that is demended
+      for dem in 100.0; do
+        #ver - adapton version that is being tested, as well as the test set.
+        for ver in arggen; do 
+          #change - particular change that is done to the list, default is all
+          for change in "--id"; do
+            fullver=Rope_mergesort_$ver
+            echo "Running: $fullver @ $len @ $samp"
+            args1="--sample-num $samp --n $len --demand $dem"
+            args2="--experiment $fullver --gran $gran"
+            args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
+            args4= #"--test-flags" #check for correctness
+            $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
+          done
+        done
+      done
+    done
+  done
+done
+
 # FILE=$FILE_PRE/mergesort_barchart.csv
 # #len - length of the list, must be a multiple of --num-changes, which has a default of 10
 # for len in 10000; do
@@ -305,141 +306,141 @@ TESTER=../experiments.native
 # done
 
 
-FILE=$FILE_PRE/list_eager_filter.csv
-#len - length of the list, must be a multiple of --num-changes, which has a default of 10
-for len in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000; do
-  #samp - sample number, is the random seed that is used to create the initial data to be modified
-  for samp in 1 2 3 4 5 6 7; do
-    #gran- granularity, average articulation chunk size is 2^gran 
-    for gran in 0; do
-      #dem - amount of the list that is demended
-      for dem in 0.000000000001 100.0; do
-        #ver - adapton version that is being tested, as well as the test set.
-        for ver in name arggen eagernoninc; do 
-          #change - particular change that is done to the list, default is all
-          for change in "--id" "--rr" "--bs"; do
-            fullver=List_eager_filter_$ver
-            echo "Running: $fullver @ $len @ $samp"
-            args1="--sample-num $samp --n $len --demand $dem"
-            args2="--experiment $fullver --gran $gran"
-            args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
-            args4= #"--test-flags" #check for correctness
-            $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
-          done
-        done
-      done
-    done
-  done
-done
+# FILE=$FILE_PRE/list_eager_filter.csv
+# #len - length of the list, must be a multiple of --num-changes, which has a default of 10
+# for len in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000; do
+#   #samp - sample number, is the random seed that is used to create the initial data to be modified
+#   for samp in 1 2 3 4 5 6 7; do
+#     #gran- granularity, average articulation chunk size is 2^gran 
+#     for gran in 0; do
+#       #dem - amount of the list that is demended
+#       for dem in 0.000000000001 100.0; do
+#         #ver - adapton version that is being tested, as well as the test set.
+#         for ver in name arggen eagernoninc; do 
+#           #change - particular change that is done to the list, default is all
+#           for change in "--id" "--rr" "--bs"; do
+#             fullver=List_eager_filter_$ver
+#             echo "Running: $fullver @ $len @ $samp"
+#             args1="--sample-num $samp --n $len --demand $dem"
+#             args2="--experiment $fullver --gran $gran"
+#             args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
+#             args4= #"--test-flags" #check for correctness
+#             $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
+#           done
+#         done
+#       done
+#     done
+#   done
+# done
 
-FILE=$FILE_PRE/list_eager_map.csv
-#len - length of the list, must be a multiple of --num-changes, which has a default of 10
-for len in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000; do
-  #samp - sample number, is the random seed that is used to create the initial data to be modified
-  for samp in 1 2 3 4 5 6 7; do
-    #gran- granularity, average articulation chunk size is 2^gran 
-    for gran in 0; do
-      #dem - amount of the list that is demended
-      for dem in 0.000000000001 100.0; do
-        #ver - adapton version that is being tested, as well as the test set.
-        for ver in name arggen eagernoninc; do 
-          #change - particular change that is done to the list, default is all
-          for change in "--id" "--rr" "--bs"; do
-            fullver=List_eager_map_$ver
-            echo "Running: $fullver @ $len @ $samp"
-            args1="--sample-num $samp --n $len --demand $dem"
-            args2="--experiment $fullver --gran $gran"
-            args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
-            args4= #"--test-flags" #check for correctness
-            $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
-          done
-        done
-      done
-    done
-  done
-done
+# FILE=$FILE_PRE/list_eager_map.csv
+# #len - length of the list, must be a multiple of --num-changes, which has a default of 10
+# for len in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000; do
+#   #samp - sample number, is the random seed that is used to create the initial data to be modified
+#   for samp in 1 2 3 4 5 6 7; do
+#     #gran- granularity, average articulation chunk size is 2^gran 
+#     for gran in 0; do
+#       #dem - amount of the list that is demended
+#       for dem in 0.000000000001 100.0; do
+#         #ver - adapton version that is being tested, as well as the test set.
+#         for ver in name arggen eagernoninc; do 
+#           #change - particular change that is done to the list, default is all
+#           for change in "--id" "--rr" "--bs"; do
+#             fullver=List_eager_map_$ver
+#             echo "Running: $fullver @ $len @ $samp"
+#             args1="--sample-num $samp --n $len --demand $dem"
+#             args2="--experiment $fullver --gran $gran"
+#             args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
+#             args4= #"--test-flags" #check for correctness
+#             $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
+#           done
+#         done
+#       done
+#     done
+#   done
+# done
 
 
-FILE=$FILE_PRE/list_reverse_sm.csv
-#len - length of the list, must be a multiple of --num-changes, which has a default of 10
-for len in 10000; do
-  #samp - sample number, is the random seed that is used to create the initial data to be modified
-  for samp in 1 2 3 4 5 6 7; do
-    #gran- granularity, average articulation chunk size is 2^gran 
-    for gran in 0; do
-      #dem - amount of the list that is demended
-      for dem in 0.000000000001 100.0; do
-        #ver - adapton version that is being tested, as well as the test set.
-        for ver in name arggen lazyrecalc; do 
-          #change - particular change that is done to the list, default is all
-          for change in "--id" "--rr" "--bs"; do
-            fullver=List_reverse_$ver
-            echo "Running: $fullver @ $len @ $samp"
-            args1="--sample-num $samp --n $len --demand $dem"
-            args2="--experiment $fullver --gran $gran"
-            args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
-            args4= #"--test-flags" #check for correctness
-            $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
-          done
-        done
-      done
-    done
-  done
-done
+# FILE=$FILE_PRE/list_reverse_sm.csv
+# #len - length of the list, must be a multiple of --num-changes, which has a default of 10
+# for len in 10000; do
+#   #samp - sample number, is the random seed that is used to create the initial data to be modified
+#   for samp in 1 2 3 4 5 6 7; do
+#     #gran- granularity, average articulation chunk size is 2^gran 
+#     for gran in 0; do
+#       #dem - amount of the list that is demended
+#       for dem in 0.000000000001 100.0; do
+#         #ver - adapton version that is being tested, as well as the test set.
+#         for ver in name arggen lazyrecalc; do 
+#           #change - particular change that is done to the list, default is all
+#           for change in "--id" "--rr" "--bs"; do
+#             fullver=List_reverse_$ver
+#             echo "Running: $fullver @ $len @ $samp"
+#             args1="--sample-num $samp --n $len --demand $dem"
+#             args2="--experiment $fullver --gran $gran"
+#             args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
+#             args4= #"--test-flags" #check for correctness
+#             $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
+#           done
+#         done
+#       done
+#     done
+#   done
+# done
 
-FILE=$FILE_PRE/rope_min_sm.csv
-#len - length of the list, must be a multiple of --num-changes, which has a default of 10
-for len in 10000; do
-  #samp - sample number, is the random seed that is used to create the initial data to be modified
-  for samp in 1 2 3 4 5 6 7; do
-    #gran- granularity, average articulation chunk size is 2^gran 
-    for gran in 0; do
-      #dem - amount of the list that is demended
-      for dem in 0.000000000001; do
-        #ver - adapton version that is being tested, as well as the test set.
-        for ver in name arggen lazyrecalc; do 
-          #change - particular change that is done to the list, default is all
-          for change in "--id" "--rr" "--bs"; do
-            fullver=Rope_min_$ver
-            echo "Running: $fullver @ $len @ $samp"
-            args1="--sample-num $samp --n $len --demand $dem"
-            args2="--experiment $fullver --gran $gran"
-            args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
-            args4= #"--test-flags" #check for correctness
-            $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
-          done
-        done
-      done
-    done
-  done
-done
+# FILE=$FILE_PRE/rope_min_sm.csv
+# #len - length of the list, must be a multiple of --num-changes, which has a default of 10
+# for len in 10000; do
+#   #samp - sample number, is the random seed that is used to create the initial data to be modified
+#   for samp in 1 2 3 4 5 6 7; do
+#     #gran- granularity, average articulation chunk size is 2^gran 
+#     for gran in 0; do
+#       #dem - amount of the list that is demended
+#       for dem in 0.000000000001; do
+#         #ver - adapton version that is being tested, as well as the test set.
+#         for ver in name arggen lazyrecalc; do 
+#           #change - particular change that is done to the list, default is all
+#           for change in "--id" "--rr" "--bs"; do
+#             fullver=Rope_min_$ver
+#             echo "Running: $fullver @ $len @ $samp"
+#             args1="--sample-num $samp --n $len --demand $dem"
+#             args2="--experiment $fullver --gran $gran"
+#             args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
+#             args4= #"--test-flags" #check for correctness
+#             $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
+#           done
+#         done
+#       done
+#     done
+#   done
+# done
 
-FILE=$FILE_PRE/rope_sum_sm.csv
-#len - length of the list, must be a multiple of --num-changes, which has a default of 10
-for len in 10000; do
-  #samp - sample number, is the random seed that is used to create the initial data to be modified
-  for samp in 1 2 3 4 5 6 7; do
-    #gran- granularity, average articulation chunk size is 2^gran 
-    for gran in 0; do
-      #dem - amount of the list that is demended
-      for dem in 0.000000000001; do
-        #ver - adapton version that is being tested, as well as the test set.
-        for ver in name arggen lazyrecalc; do 
-          #change - particular change that is done to the list, default is all
-          for change in "--id" "--rr" "--bs"; do
-            fullver=Rope_sum_$ver
-            echo "Running: $fullver @ $len @ $samp"
-            args1="--sample-num $samp --n $len --demand $dem"
-            args2="--experiment $fullver --gran $gran"
-            args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
-            args4= #"--test-flags" #check for correctness
-            $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
-          done
-        done
-      done
-    done
-  done
-done
+# FILE=$FILE_PRE/rope_sum_sm.csv
+# #len - length of the list, must be a multiple of --num-changes, which has a default of 10
+# for len in 10000; do
+#   #samp - sample number, is the random seed that is used to create the initial data to be modified
+#   for samp in 1 2 3 4 5 6 7; do
+#     #gran- granularity, average articulation chunk size is 2^gran 
+#     for gran in 0; do
+#       #dem - amount of the list that is demended
+#       for dem in 0.000000000001; do
+#         #ver - adapton version that is being tested, as well as the test set.
+#         for ver in name arggen lazyrecalc; do 
+#           #change - particular change that is done to the list, default is all
+#           for change in "--id" "--rr" "--bs"; do
+#             fullver=Rope_sum_$ver
+#             echo "Running: $fullver @ $len @ $samp"
+#             args1="--sample-num $samp --n $len --demand $dem"
+#             args2="--experiment $fullver --gran $gran"
+#             args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
+#             args4= #"--test-flags" #check for correctness
+#             $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
+#           done
+#         done
+#       done
+#     done
+#   done
+# done
 
 
 
