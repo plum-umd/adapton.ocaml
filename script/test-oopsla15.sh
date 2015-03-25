4d05834dec0331249b67d15a4892bee7f8336709
Kyle Headley <kheadley@umd.edu>
2015-03-25 11:20:52 -0400

    possibly add final numbers

--- script/test-oopsla15.sh
+++ script/test-oopsla15.sh
@@ -251,33 +251,60 @@ TESTER=../experiments.native
 #   done
 # done
 
-FILE=$FILE_PRE/mergesort_linechart.csv
-#len - length of the list, must be a multiple of --num-changes, which has a default of 10
-for len in 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000; do
-  #samp - sample number, is the random seed that is used to create the initial data to be modified
-  for samp in 1 2 3 4 5 6 7; do
-    #gran- granularity, average articulation chunk size is 2^gran 
-    for gran in 0; do
-      #dem - amount of the list that is demended
-      for dem in 100.0; do
-        #ver - adapton version that is being tested, as well as the test set.
-        for ver in name lazyrecalc; do 
-          #change - particular change that is done to the list, default is all
-          for change in "--id"; do
-            fullver=Rope_mergesort_$ver
-            echo "Running: $fullver @ $len @ $samp"
-            args1="--sample-num $samp --n $len --demand $dem"
-            args2="--experiment $fullver --gran $gran"
-            args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
-            args4= #"--test-flags" #check for correctness
-            $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
-          done
-        done
-      done
-    done
-  done
-done
-FILE=$FILE_PRE/mergesort_barchart.csv
+# FILE=$FILE_PRE/mergesort_linechart.csv
+# #len - length of the list, must be a multiple of --num-changes, which has a default of 10
+# for len in 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000; do
+#   #samp - sample number, is the random seed that is used to create the initial data to be modified
+#   for samp in 1 2 3 4 5 6 7; do
+#     #gran- granularity, average articulation chunk size is 2^gran 
+#     for gran in 0; do
+#       #dem - amount of the list that is demended
+#       for dem in 100.0; do
+#         #ver - adapton version that is being tested, as well as the test set.
+#         for ver in name lazyrecalc; do 
+#           #change - particular change that is done to the list, default is all
+#           for change in "--id"; do
+#             fullver=Rope_mergesort_$ver
+#             echo "Running: $fullver @ $len @ $samp"
+#             args1="--sample-num $samp --n $len --demand $dem"
+#             args2="--experiment $fullver --gran $gran"
+#             args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
+#             args4= #"--test-flags" #check for correctness
+#             $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
+#           done
+#         done
+#       done
+#     done
+#   done
+# done
+# FILE=$FILE_PRE/mergesort_barchart.csv
+# #len - length of the list, must be a multiple of --num-changes, which has a default of 10
+# for len in 10000; do
+#   #samp - sample number, is the random seed that is used to create the initial data to be modified
+#   for samp in 1 2 3 4 5 6 7; do
+#     #gran- granularity, average articulation chunk size is 2^gran 
+#     for gran in 0; do
+#       #dem - amount of the list that is demended
+#       for dem in 100.0; do
+#         #ver - adapton version that is being tested, as well as the test set.
+#         for ver in name arggen lazyrecalc; do 
+#           #change - particular change that is done to the list, default is all
+#           for change in "--rr --id --ss --bs"; do
+#             fullver=Rope_mergesort_$ver
+#             echo "Running: $fullver @ $len @ $samp"
+#             args1="--sample-num $samp --n $len --demand $dem"
+#             args2="--experiment $fullver --gran $gran"
+#             args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
+#             args4= #"--test-flags" #check for correctness
+#             $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
+#           done
+#         done
+#       done
+#     done
+#   done
+# done
+
+FILE=$FILE_PRE/quickhull.csv
 #len - length of the list, must be a multiple of --num-changes, which has a default of 10
 for len in 10000; do
   #samp - sample number, is the random seed that is used to create the initial data to be modified
@@ -285,12 +312,12 @@ for len in 10000; do
     #gran- granularity, average articulation chunk size is 2^gran 
     for gran in 0; do
       #dem - amount of the list that is demended
-      for dem in 100.0; do
+      for dem in 0.000000000001 100.0; do
         #ver - adapton version that is being tested, as well as the test set.
-        for ver in name arggen lazyrecalc; do 
+        for ver in name lazyrecalc; do 
           #change - particular change that is done to the list, default is all
-          for change in "--rr --id --ss --bs"; do
-            fullver=Rope_mergesort_$ver
+          for change in "--bs"; do
+            fullver=Quickhull_$ver
             echo "Running: $fullver @ $len @ $samp"
             args1="--sample-num $samp --n $len --demand $dem"
             args2="--experiment $fullver --gran $gran"
@@ -303,4 +330,28 @@ for len in 10000; do
     done
   done
 done
-
+#for Quickhull_arggen, we need to run at a lower length
+for len in 1000; do
+  #samp - sample number, is the random seed that is used to create the initial data to be modified
+  for samp in 1 2 3 4 5 6 7; do
+    #gran- granularity, average articulation chunk size is 2^gran 
+    for gran in 0; do
+      #dem - amount of the list that is demended
+      for dem in 0.000000000001 100.0; do
+        #ver - adapton version that is being tested, as well as the test set.
+        for ver in arggen; do 
+          #change - particular change that is done to the list, default is all
+          for change in "--bs"; do
+            fullver=Quickhull_$ver
+            echo "Running: $fullver @ $len @ $samp"
+            args1="--sample-num $samp --n $len --demand $dem"
+            args2="--experiment $fullver --gran $gran"
+            args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
+            args4= #"--test-flags" #check for correctness
+            $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
+          done
+        done
+      done
+    done
+  done
+done
