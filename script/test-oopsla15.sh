ecdd6de773f493083c9b31701bc6af33c3d81cb8
Kyle Headley <kheadley@umd.edu>
2015-03-25 22:38:45 -0400

    get the baseline nums for quickhull arggen

--- script/test-oopsla15.sh
+++ script/test-oopsla15.sh
@@ -225,46 +225,20 @@ TESTER=../experiments.native
 #     done
 #   done
 # done
-# #for Quickhull_arggen, we need to run at a lower length
-# for len in 1000; do
-#   #samp - sample number, is the random seed that is used to create the initial data to be modified
-#   for samp in 1 2 3 4 5 6 7; do
-#     #gran- granularity, average articulation chunk size is 2^gran 
-#     for gran in 0; do
-#       #dem - amount of the list that is demended
-#       for dem in 0.000000000001 100.0; do
-#         #ver - adapton version that is being tested, as well as the test set.
-#         for ver in arggen; do 
-#           #change - particular change that is done to the list, default is all
-#           for change in "--id" "--rr"; do
-#             fullver=Quickhull_$ver
-#             echo "Running: $fullver @ $len @ $samp"
-#             args1="--sample-num $samp --n $len --demand $dem"
-#             args2="--experiment $fullver --gran $gran"
-#             args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
-#             args4= #"--test-flags" #check for correctness
-#             $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
-#           done
-#         done
-#       done
-#     done
-#   done
-# done
-
-FILE=$FILE_PRE/mergesort_linechart.csv
-#len - length of the list, must be a multiple of --num-changes, which has a default of 10
-for len in 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000; do
+#for Quickhull_arggen, we need to run at a lower length
+FILE=$FILE_PRE/quickhull_lazyfix.csv
+for len in 1000; do
   #samp - sample number, is the random seed that is used to create the initial data to be modified
   for samp in 1 2 3 4 5 6 7; do
     #gran- granularity, average articulation chunk size is 2^gran 
     for gran in 0; do
       #dem - amount of the list that is demended
-      for dem in 100.0; do
+      for dem in 0.000000000001 100.0; do
         #ver - adapton version that is being tested, as well as the test set.
-        for ver in arggen; do 
+        for ver in lazyrecalc; do #arggen; do 
           #change - particular change that is done to the list, default is all
-          for change in "--id"; do
-            fullver=Rope_mergesort_$ver
+          for change in "--id" "--rr"; do
+            fullver=Quickhull_$ver
             echo "Running: $fullver @ $len @ $samp"
             args1="--sample-num $samp --n $len --demand $dem"
             args2="--experiment $fullver --gran $gran"
@@ -277,6 +251,33 @@ for len in 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000; do
     done
   done
 done
+
+# FILE=$FILE_PRE/mergesort_linechart.csv
+# #len - length of the list, must be a multiple of --num-changes, which has a default of 10
+# for len in 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000; do
+#   #samp - sample number, is the random seed that is used to create the initial data to be modified
+#   for samp in 1 2 3 4 5 6 7; do
+#     #gran- granularity, average articulation chunk size is 2^gran 
+#     for gran in 0; do
+#       #dem - amount of the list that is demended
+#       for dem in 100.0; do
+#         #ver - adapton version that is being tested, as well as the test set.
+#         for ver in arggen; do 
+#           #change - particular change that is done to the list, default is all
+#           for change in "--id"; do
+#             fullver=Rope_mergesort_$ver
+#             echo "Running: $fullver @ $len @ $samp"
+#             args1="--sample-num $samp --n $len --demand $dem"
+#             args2="--experiment $fullver --gran $gran"
+#             args3="--num-changes 10 --0 $change" #one change type at a time, on new copy of list
+#             args4= #"--test-flags" #check for correctness
+#             $TESTER $args1 $args2 $args3 $args4 --outfile $FILE
+#           done
+#         done
+#       done
+#     done
+#   done
+# done
 
 # FILE=$FILE_PRE/mergesort_barchart.csv
 # #len - length of the list, must be a multiple of --num-changes, which has a default of 10
