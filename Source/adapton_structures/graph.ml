f33eb0714c7a4d0b4f0a61aac5c53f7fc6a2f024
Hammer <hammer@cs.umd.edu>
2015-03-29 01:14:09 -0400

    graph construction

--- Source/adapton_structures/graph.ml
+++ Source/adapton_structures/graph.ml
@@ -18,12 +18,34 @@ module Make
     module Edge = AdaptonTypes.Tuple3(NodeId)(NodeId)(EdgeData)
     module Adj  = AdaptonTypes.Tuple2(NodeId)(EdgeData)
     module AdjSt = SpreadTree.MakeSpreadTree(ArtLib)(Name)(Adj)
-    module NodeVal = AdaptonTypes.Tuple2(NodeData)(AdjSt.List.Data)
+    module NodeVal = AdaptonTypes.Tuple2
+                       (AdaptonTypes.Option(NodeData))
+                       (AdjSt.List.Data)
                                      
     module NodeMap = Trie.Map.Make(Trie.Useful(NodeId))
                                   (Trie.Useful(NodeVal))
                                   (Name)(ArtLib)
 
+    type t = NodeMap.t
 
-end
+    let empty = NodeMap.empty
+
+    let name_adjs =
+      (AdjSt.List.Art.mk_mfn
+        (Name.gensym "Graph.name_cons")
+        (module AdjSt.List.Data)
+        (fun _ adj_list -> adj_list))
+        .AdjSt.List.Art
+        .mfn_nart
+
+    let add_edge graph nm a b ab_data =
+      match NodeMap.find graph a with
+      | None ->
+         NodeMap.nadd nm graph a (None,`Cons((b,ab_data),`Nil))
+      | Some (data, adjs) ->
+         let nm1,nm2 = Name.fork nm in
+         let adjs = `Art(name_adjs nm2 adjs) in
+         NodeMap.nadd nm1 graph a (data,`Cons((b,ab_data),adjs))
+
+  end
 
