3ccca33f7e86ca4ee974f0a7d0b1de563948746f
Hammer <hammer@cs.umd.edu>
2015-04-20 15:11:37 -0400

    forgot

--- script/search-samples.sh
+++ script/search-samples.sh
@@ -1,11 +1,13 @@
 while [[ 1 ]]; do
-    s=$((s + 1))
-    cmd="./experiments.native --experiment Rope_mergesort_sac --sample-num $s --n 8 --num-changes 7 --test-flags"
-    echo ------------------------------------------------------------------------------------------------------------
-    echo `date`
-    echo Sample num: $s
-    echo Command: $cmd
-    echo ------------------------------------------------------------------------------------------------------------ >> $0.out
-    echo $cmd >> $0.out
-    $cmd >> $0.out || exit
+    for edit in --r --id --di; do
+        s=$((s + 1))
+        cmd="./experiments.native --experiment Rope_mergesort_sac --sample-num $s --n 8 --num-changes 7 --0 $edit --test-flags"
+        echo ------------------------------------------------------------------------------------------------------------
+        echo `date`
+        echo Sample num: $s
+        echo Command: $cmd
+        echo ------------------------------------------------------------------------------------------------------------ >> $0.out
+        echo $cmd >> $0.out
+        $cmd >> $0.out || exit
+    done
 done
