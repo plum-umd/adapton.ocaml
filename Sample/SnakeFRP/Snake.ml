1d83e5a27c3fcc5657566d5ce9c221306a70e8d3
Kyle Headley <kheadley@umd.edu>
2015-06-16 18:03:06 -0400

    begin to add adapton types to sim

--- Sample/SnakeFRP/Snake.ml
+++ Sample/SnakeFRP/Snake.ml
@@ -11,6 +11,9 @@ open Adapton.Structures.SpreadTree
 module PointST = MakeSpreadTree(Adapton.NameLib)(N)(Point)
 module PointSeq = MakeSeq(PointST)
 
+(* setup adapton for simple temp sim *)
+module AInt = Adapton.NameLib.MakeArt(N)(T.Int)
+
 (* define streams *)
 type 'a stream = {
   mutable data : 'a;                     (* stream's current value *)
@@ -42,45 +45,94 @@ let f_birthrate = 10
 let f_deathrate = 30
 let f_high_population = 200
 
-(* setup streams *)
-let rec rabbits = 
-  { update = (fun _ -> ());
-    data = r_initial;
-    collect = fun () ->
-      let r = rabbits.data in
-      let f = foxes.data in
+let rabbit_updater =
+  let memo = AInt.mk_mfn (N.gensym "rabbit updater")
+    (module T.Tuple2(AInt)(AInt))
+    (fun _ (rs, fs) ->
+      let r = AInt.force rs in
+      let f = AInt.force fs in
       let predation = min (f * hunting_max / 100) (r - (r * survival / 100)) in
       let newr = r + (r * (r_birthrate - r_deathrate * r / r_high_population) / 100) - (predation) in
-      fun s ->
-        s.data <- newr
-  }
-and foxes =
-  { update = (fun _ -> ());
-    data = f_initial;
-    collect = fun () ->
-      let r = rabbits.data in
-      let f = foxes.data in
-      let predation = min (f * hunting_max / 100) (r - (r * survival / 100)) in
-      let extra_food = predation * nutrition / 100 in
-      let newf = f + (f * (f_birthrate - f_deathrate * f / f_high_population) / 100) + extra_food in
-      fun s -> s.data <- newf
-  }
-and output =
-  { update = (fun _ -> ());
-    data = 0;
-    collect = fun () ->
-      let r = rabbits.data in
-      let f = foxes.data in
-      Printf.printf "Rabbits: %d, Foxes: %d\n" r f;
-      fun _ -> ()
-  }
-and tick_counter =
-  { update = (fun _ -> ());
-    data = 0;
-    collect = fun () ->
-      let v = if tick_counter.data > 20 then exit 0 else tick_counter.data + 1 in
-      fun s -> s.data <- v
-  }
+      newr
+    )
+    in
+    fun rs fs -> memo.AInt.mfn_data (rs,fs)
+
+(* same as above, and same non-adapton version, see note in comment below *)
+let fox_updater rs fs =
+    let r = AInt.force rs.data in
+    let f = AInt.force fs.data in
+    let predation = min (f * hunting_max / 100) (r - (r * survival / 100)) in
+    let extra_food = predation * nutrition / 100 in
+    let newf = f + (f * (f_birthrate - f_deathrate * f / f_high_population) / 100) + extra_food in
+    newf
+
+(* setup streams *)
+let rec rabbits = {
+  update = (fun _ -> ());
+  data = AInt.cell (N.nondet()) r_initial;
+  collect = fun () ->
+    let r = AInt.force rabbits.data in
+    let f = AInt.force foxes.data in
+    let predation = min (f * hunting_max / 100) (r - (r * survival / 100)) in
+    let newr = r + (r * (r_birthrate - r_deathrate * r / r_high_population) / 100) - (predation) in
+    fun s ->
+      AInt.set s.data newr
+}
+and a_rabbits = {
+  update = (fun _ -> ());
+  data = AInt.cell (N.nondet()) r_initial;
+  collect = fun () ->
+    let newr = rabbit_updater a_rabbits.data a_foxes.data in
+    fun s -> AInt.set s.data newr
+}
+and foxes = {
+  update = (fun _ -> ());
+  data = AInt.cell (N.nondet()) f_initial;
+  collect = fun () ->
+    let r = AInt.force rabbits.data in
+    let f = AInt.force foxes.data in
+    let predation = min (f * hunting_max / 100) (r - (r * survival / 100)) in
+    let extra_food = predation * nutrition / 100 in
+    let newf = f + (f * (f_birthrate - f_deathrate * f / f_high_population) / 100) + extra_food in
+    fun s ->
+      AInt.set s.data newf
+}
+(* this kind of expression is not allowed on the right side of 'let rec': recursive value as param to function call
+and fox_updater = AInt.thunk (N.nondet()) (fun () ->
+  let r = AInt.force a_rabbits.data in
+  let f = AInt.force a_foxes.data in
+  let predation = min (f * hunting_max / 100) (r - (r * survival / 100)) in
+  let extra_food = predation * nutrition / 100 in
+  let newf = f + (f * (f_birthrate - f_deathrate * f / f_high_population) / 100) + extra_food in
+  newf
+)
+*)
+and a_foxes = {
+  update = (fun _ -> ());
+  data = AInt.cell (N.nondet()) f_initial;
+  collect = fun () ->
+    let newf = fox_updater a_rabbits a_foxes in
+    fun s -> AInt.set s.data newf
+}
+and output = {
+  update = (fun _ -> ());
+  data = AInt.cell (N.nondet()) 0;
+  collect = fun () ->
+    let r = AInt.force rabbits.data in
+    let f = AInt.force foxes.data in
+    let ar = AInt.force rabbits.data in
+    let af = AInt.force foxes.data in
+    Printf.printf "Rabbits: %d|%d, Foxes: %d|%d\n" r ar f af;
+    fun _ -> ()
+}
+and tick_counter = {
+  update = (fun _ -> ());
+  data = AInt.cell (N.nondet()) 0;
+  collect = fun () ->
+    let v = if AInt.force tick_counter.data > 20 then exit 0 else AInt.force tick_counter.data + 1 in
+    fun s -> AInt.set s.data v
+}
 and streams = [ rabbits; foxes; output; tick_counter ]
 
 (* go! *)
